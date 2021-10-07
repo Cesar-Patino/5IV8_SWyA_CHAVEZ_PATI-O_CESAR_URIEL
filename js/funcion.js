bdb20c91a357db089acb67db7353737c63bc563d
unknown <cchavezp1900@alumno.ipn.mx>
2021-10-07 14:02:39 -0500

    yaa

--- js/funcion.js
+++ js/funcion.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
         $('ul.tabs li a').removeClass('active');
         $(this).addClass('active');
         $('.secciones article').hide();
-
         var activeTab = $(this).attr('href');
         $(activeTab).show();
         return false;
